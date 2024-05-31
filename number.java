f7802111b76feb072ca1de0c8f92ec7cfc4d05a7
Yogesh  Kumar soni <153350265+bcacoders2023@users.noreply.github.com>
2024-05-31 12:54:17 +0530

    Add files via upload

---
+++ number.java
@@ -0,0 +1,15 @@
+// WAP to prints all the number from 1-10 specified range using a for loop.
+import java.util.*;
+public class number {
+
+    public static void main(String[] args) {
+        System.out.println("ragne");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0; i<n; i++)
+        {
+            System.out.println(i);
+        }
+ 
+    }
+}
